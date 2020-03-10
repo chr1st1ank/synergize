2184f314c79aeafd43e254a8d6f4eae27d50c082
chr1st1ank <chr1st1ank@krudewig-online.de>
2020-03-10 23:41:00 +0100

    Considering existing hardlinks

--- internal/synergize/main.go
+++ internal/synergize/main.go
@@ -3,5 +3,5 @@ package synergize
 func RunPipeline(targetFolder string) {
 	foundFilesChannel := ScanTree(targetFolder, 10)
 	hashedFilesChannel := TakeFileFingerprint(foundFilesChannel, 5)
-	SkipOrSynergizeFile(hashedFilesChannel)
+	SkipOrSynergizeFile(hashedFilesChannel, SynergizeFile)
 }
--- internal/synergize/skiporsynergizefile.go
+++ internal/synergize/skiporsynergizefile.go
@@ -5,7 +5,7 @@ import (
 	"github.com/chr1st1ank/synergize/internal/fs"
 )
 
-func SkipOrSynergizeFile(fi <-chan fs.FileOnDiskInfo) {
+func SkipOrSynergizeFile(fi <-chan fs.FileOnDiskInfo, synergizeFunction func(string, string)) {
 	filesByHash := make(map[string]fs.FileOnDiskInfo, 100)
 	for nextFile := range fi {
 		hash, err := nextFile.FileInfo.Hash()
@@ -16,8 +16,15 @@ func SkipOrSynergizeFile(fi <-chan fs.FileOnDiskInfo) {
 		if !known {
 			fmt.Println(nextFile.FileInfo.Path(), hash, " (new)")
 			filesByHash[hash] = nextFile
+		} else if otherFile.InodeInfo.Address() == nextFile.InodeInfo.Address() {
+			fmt.Println(nextFile.FileInfo.Path(), hash, " (already hardlinked to", otherFile.FileInfo.Path(), ")")
 		} else {
 			fmt.Println(nextFile.FileInfo.Path(), hash, " (duplicate of ", otherFile.FileInfo.Path(), ")")
+			SynergizeFile(nextFile.FileInfo.Path(), otherFile.FileInfo.Path())
 		}
 	}
+}
+
+func SynergizeFile(fileToReplacePath string, linkTargetPath string) {
+	fmt.Println("Deleting", fileToReplacePath, "and replacing with a link to", linkTargetPath)
 }
